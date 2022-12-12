595830cfe0812a571bc686441d92555a924420c8
DmitryProshin <do.proshin@yandex.ru>
2022-12-12 20:47:28 +0500

    update keyboard

--- src/main/java/keyboards/Keyboard.java
+++ src/main/java/keyboards/Keyboard.java
@@ -1,4 +1,52 @@
 package keyboards;
 
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Keyboard {
+
+    private List<String> commands = new ArrayList<>();
+
+    public Keyboard() {
+        this.commands.add("/help");
+        this.commands.add("/list_of_teams");
+    }
+    public void setButtons(SendMessage sendMessage) {
+
+        // Create a keyboard
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup();
+        sendMessage.setReplyMarkup(replyKeyboardMarkup);
+        replyKeyboardMarkup.setSelective(true);
+        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(false);
+
+        // Create a list of keyboard rows
+        List keyboard = new ArrayList<>();
+        // First keyboard row
+        KeyboardRow keyboardFirstRow = new KeyboardRow();
+
+        // Add buttons to the first keyboard row
+        keyboardFirstRow.add(new KeyboardButton("/help"));
+
+        // Second keyboard row
+        KeyboardRow keyboardSecondRow = new KeyboardRow();
+        // Add the buttons to the second keyboard row
+        keyboardSecondRow.add(new KeyboardButton("/list_of_teams"));
+
+        // Add all of the keyboard rows to the list
+        keyboard.add(keyboardFirstRow);
+        keyboard.add(keyboardSecondRow);
+        // and assign this list to our keyboard
+        replyKeyboardMarkup.setKeyboard(keyboard);
+    }
+
+    public Boolean inCommand(String text){
+        if (commands.contains(text)) return true;
+        else return false;
+    }
 }
