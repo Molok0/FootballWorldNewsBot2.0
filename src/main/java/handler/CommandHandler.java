bf89737070f462a79de7fe17de57b99ed7775d9a
DmitryProshin <do.proshin@yandex.ru>
2022-12-12 20:59:39 +0500

    update CommandHandler

--- src/main/java/handler/CommandHandler.java
+++ src/main/java/handler/CommandHandler.java
@@ -1,2 +1,21 @@
-package handler;public class CommandHandler {
+package handler;
+
+import Bot.TeamsList;
+
+public class CommandHandler {
+
+    private String response;
+
+    public String getResponse(String text, TeamsList teamsList){
+        switch (text){
+            case "/help":
+                response = "Этот бот выводит новости про нужную вам футбольныую команду. Введите название команды и бот выдаст последние новости.";
+                break;
+            case "/list_of_teams":
+                response = teamsList.toString();
+                break;
+        }
+
+        return response;
+    }
 }
